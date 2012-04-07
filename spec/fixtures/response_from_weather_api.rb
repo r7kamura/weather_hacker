e43ad001b5936b923b4f00791c572325beea7474
Ryo NAKAMURA <r7kamura@gmail.com>
2012-04-08 01:50:37 +0900

    Add sample data from API

---
+++ spec/fixtures/response_from_weather_api.rb
@@ -0,0 +1,327 @@
+{
+  "lwws" => {
+          "author" => "livedoor Weather Team.",
+        "location" => {
+      "area" => "関東",
+      "pref" => "東京都",
+      "city" => "東京"
+    },
+           "title" => "東京都 東京 - 今日の天気",
+            "link" => "http://weather.livedoor.com/area/13/63.html?v=1",
+     "forecastday" => "today",
+             "day" => "Saturday",
+    "forecastdate" => "Sat, 07 Apr 2012 00:00:00 +0900",
+      "publictime" => "Sat, 07 Apr 2012 17:00:00 +0900",
+           "telop" => "晴時々曇",
+     "description" => "伊豆諸島南部では強風に、伊豆諸島北部、伊豆諸島南部では高波に、東京地方、伊豆諸島南部では空気の乾燥による火の取り扱いに、伊豆諸島北部では霜に対する農作...<br />[PR]<a href=\"http://weather.livedoor.com/indexes/cloth_wash/?r=rest_pr\">きょうは洗濯できるかな？</a>",
+           "image" => {
+       "title" => "晴時々曇",
+        "link" => "http://weather.livedoor.com/area/13/63.html?v=1",
+         "url" => "http://image.weather.livedoor.com/img/icon/2.gif",
+       "width" => "50",
+      "height" => "31"
+    },
+     "temperature" => {
+      "max" => {
+           "celsius" => nil,
+        "fahrenheit" => nil
+      },
+      "min" => {
+           "celsius" => nil,
+        "fahrenheit" => nil
+      }
+    },
+        "pinpoint" => {
+      "location" => [
+        {
+               "title" => "千代田区",
+                "link" => "http://weather.livedoor.com/p/13/63/13101.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        },
+        {
+               "title" => "中央区",
+                "link" => "http://weather.livedoor.com/p/13/63/13102.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        },
+        {
+               "title" => "港区",
+                "link" => "http://weather.livedoor.com/p/13/63/13103.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        },
+        {
+               "title" => "新宿区",
+                "link" => "http://weather.livedoor.com/p/13/63/13104.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        },
+        {
+               "title" => "文京区",
+                "link" => "http://weather.livedoor.com/p/13/63/13105.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        },
+        {
+               "title" => "台東区",
+                "link" => "http://weather.livedoor.com/p/13/63/13106.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        },
+        {
+               "title" => "墨田区",
+                "link" => "http://weather.livedoor.com/p/13/63/13107.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        },
+        {
+               "title" => "江東区",
+                "link" => "http://weather.livedoor.com/p/13/63/13108.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        },
+        {
+               "title" => "品川区",
+                "link" => "http://weather.livedoor.com/p/13/63/13109.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        },
+        {
+               "title" => "目黒区",
+                "link" => "http://weather.livedoor.com/p/13/63/13110.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        },
+        {
+               "title" => "大田区",
+                "link" => "http://weather.livedoor.com/p/13/63/13111.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        },
+        {
+               "title" => "世田谷区",
+                "link" => "http://weather.livedoor.com/p/13/63/13112.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        },
+        {
+               "title" => "渋谷区",
+                "link" => "http://weather.livedoor.com/p/13/63/13113.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        },
+        {
+               "title" => "中野区",
+                "link" => "http://weather.livedoor.com/p/13/63/13114.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        },
+        {
+               "title" => "杉並区",
+                "link" => "http://weather.livedoor.com/p/13/63/13115.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        },
+        {
+               "title" => "豊島区",
+                "link" => "http://weather.livedoor.com/p/13/63/13116.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        },
+        {
+               "title" => "北区",
+                "link" => "http://weather.livedoor.com/p/13/63/13117.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        },
+        {
+               "title" => "荒川区",
+                "link" => "http://weather.livedoor.com/p/13/63/13118.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        },
+        {
+               "title" => "板橋区",
+                "link" => "http://weather.livedoor.com/p/13/63/13119.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        },
+        {
+               "title" => "練馬区",
+                "link" => "http://weather.livedoor.com/p/13/63/13120.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        },
+        {
+               "title" => "足立区",
+                "link" => "http://weather.livedoor.com/p/13/63/13121.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        },
+        {
+               "title" => "葛飾区",
+                "link" => "http://weather.livedoor.com/p/13/63/13122.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        },
+        {
+               "title" => "江戸川区",
+                "link" => "http://weather.livedoor.com/p/13/63/13123.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        },
+        {
+               "title" => "八王子市",
+                "link" => "http://weather.livedoor.com/p/13/63/13201.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        },
+        {
+               "title" => "立川市",
+                "link" => "http://weather.livedoor.com/p/13/63/13202.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        },
+        {
+               "title" => "武蔵野市",
+                "link" => "http://weather.livedoor.com/p/13/63/13203.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        },
+        {
+               "title" => "三鷹市",
+                "link" => "http://weather.livedoor.com/p/13/63/13204.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        },
+        {
+               "title" => "青梅市",
+                "link" => "http://weather.livedoor.com/p/13/63/13205.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        },
+        {
+               "title" => "府中市",
+                "link" => "http://weather.livedoor.com/p/13/63/13206.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        },
+        {
+               "title" => "昭島市",
+                "link" => "http://weather.livedoor.com/p/13/63/13207.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        },
+        {
+               "title" => "調布市",
+                "link" => "http://weather.livedoor.com/p/13/63/13208.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        },
+        {
+               "title" => "町田市",
+                "link" => "http://weather.livedoor.com/p/13/63/13209.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        },
+        {
+               "title" => "小金井市",
+                "link" => "http://weather.livedoor.com/p/13/63/13210.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        },
+        {
+               "title" => "小平市",
+                "link" => "http://weather.livedoor.com/p/13/63/13211.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        },
+        {
+               "title" => "日野市",
+                "link" => "http://weather.livedoor.com/p/13/63/13212.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        },
+        {
+               "title" => "東村山市",
+                "link" => "http://weather.livedoor.com/p/13/63/13213.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        },
+        {
+               "title" => "国分寺市",
+                "link" => "http://weather.livedoor.com/p/13/63/13214.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        },
+        {
+               "title" => "国立市",
+                "link" => "http://weather.livedoor.com/p/13/63/13215.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        },
+        {
+               "title" => "福生市",
+                "link" => "http://weather.livedoor.com/p/13/63/13218.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        },
+        {
+               "title" => "狛江市",
+                "link" => "http://weather.livedoor.com/p/13/63/13219.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        },
+        {
+               "title" => "東大和市",
+                "link" => "http://weather.livedoor.com/p/13/63/13220.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        },
+        {
+               "title" => "清瀬市",
+                "link" => "http://weather.livedoor.com/p/13/63/13221.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        },
+        {
+               "title" => "東久留米市",
+                "link" => "http://weather.livedoor.com/p/13/63/13222.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        },
+        {
+               "title" => "武蔵村山市",
+                "link" => "http://weather.livedoor.com/p/13/63/13223.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        },
+        {
+               "title" => "多摩市",
+                "link" => "http://weather.livedoor.com/p/13/63/13224.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        },
+        {
+               "title" => "稲城市",
+                "link" => "http://weather.livedoor.com/p/13/63/13225.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        },
+        {
+               "title" => "羽村市",
+                "link" => "http://weather.livedoor.com/p/13/63/13227.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        },
+        {
+               "title" => "あきる野市",
+                "link" => "http://weather.livedoor.com/p/13/63/13228.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        },
+        {
+               "title" => "西東京市",
+                "link" => "http://weather.livedoor.com/p/13/63/13229.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        },
+        {
+               "title" => "瑞穂町",
+                "link" => "http://weather.livedoor.com/p/13/63/13303.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        },
+        {
+               "title" => "日の出町",
+                "link" => "http://weather.livedoor.com/p/13/63/13305.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        },
+        {
+               "title" => "檜原村",
+                "link" => "http://weather.livedoor.com/p/13/63/13307.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        },
+        {
+               "title" => "奥多摩町",
+                "link" => "http://weather.livedoor.com/p/13/63/13308.html",
+          "publictime" => "Sat, 07 Apr 2012 18:00:00 +0900"
+        }
+      ]
+    },
+       "copyright" => {
+         "title" => "Copyright (C) 1996-2012 livedoor Co.,Ltd. All rights reserved.",
+          "link" => "http://weather.livedoor.com",
+         "image" => {
+         "title" => "livedoor 天気情報",
+          "link" => "http://weather.livedoor.com",
+           "url" => "http://image.weather.livedoor.com/img/cmn/livedoor.gif",
+         "width" => "118",
+        "height" => "26"
+      },
+      "provider" => [
+        {
+          "name" => "（株）ハレックス",
+          "link" => "http://www.halex.co.jp/halexbrain/weather/"
+        },
+        {
+          "name" => "日本気象協会",
+          "link" => "http://tenki.jp/"
+        }
+      ]
+    },
+         "version" => "livedoor Weather Web Service 1.0"
+  }
+}
+
