82459160878636a4683a722407a836f05661918b
Ryo NAKAMURA <r7kamura@gmail.com>
2012-04-08 20:26:50 +0900

    Implement WeatherHacker#today(tomorrow, day_after_tomorrow)

--- lib/weather_hacker.rb
+++ lib/weather_hacker.rb
@@ -7,4 +7,14 @@ class WeatherHacker
   def initialize
     @client = Client.new
   end
+
+  # define these methods
+  # * WeatherHacker#today(zipcode)
+  # * WeatherHacker#tomorrow(zipcode)
+  # * WeatherHacker#dayaftertomorrow(zipcode)
+  [:today, :tomorrow, :day_after_tomorrow].each do |day|
+    define_method(day) do |zipcode|
+      @client.get_weather(zipcode, :day => day.to_s.tr("_", ""))
+    end
+  end
 end
--- spec/weather_hacker_spec.rb
+++ spec/weather_hacker_spec.rb
@@ -14,5 +14,36 @@ describe "WeatherHacker" do
     end
   end
 
+  describe "#today" do
+    before do
+      @result = @weather.today(@zipcode)
+    end
+    it "should return weather data on today" do
+      @result.should be_kind_of Hash
+      @result.should have_key "weather"
+      @result.should have_key "temperature"
+    end
+  end
+
+  describe "#tomorrow" do
+    before do
+      @result = @weather.tomorrow(@zipcode)
+    end
+    it "should return weather data on tomorrow" do
+      @result.should be_kind_of Hash
+      @result.should have_key "weather"
+      @result.should have_key "temperature"
+    end
+  end
+
+  describe "#day_after_tomorrow" do
+    before do
+      @result = @weather.day_after_tomorrow(@zipcode)
+    end
+    it "should return weather data on day ofter tomorrow" do
+      @result.should be_kind_of Hash
+      @result.should have_key "weather"
+      @result.should have_key "temperature"
+    end
   end
 end
