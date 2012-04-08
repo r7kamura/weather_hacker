53d69d9b0c43d894e6be27a33e2dfaa74ed2c5a3
Ryo NAKAMURA <r7kamura@gmail.com>
2012-04-08 23:22:46 +0900

    Refacor: abstract specs to DRY

--- spec/weather_hacker/client_spec.rb
+++ spec/weather_hacker/client_spec.rb
@@ -27,16 +27,39 @@ describe "WeatherHacker::Client" do
     end
   end
 
-  describe "#update_area_table" do
-    it "has not table before call" do
+  shared_examples_for "having area table" do
+    it "has area table" do
+      @client.send(:id_by_city).keys.size.should_not == 0
+      @client.send(:pref_by_city).keys.size.should_not == 0
+    end
+  end
+
+  shared_examples_for "not having area table" do
+    it "has not area table" do
       @client.instance_variable_get(:@id_by_city).should be_nil
       @client.instance_variable_get(:@pref_by_city).should be_nil
     end
+  end
 
-    it "has area table after call" do
-      @client.send(:update_area_table)
-      @client.send(:id_by_city).keys.size.should_not == 0
-      @client.send(:pref_by_city).keys.size.should_not == 0
+  describe "#id_by_city" do
+    context "before call" do
+      it_behaves_like "not having area table"
+    end
+
+    context "after call" do
+      before { @client.send(:id_by_city) }
+      it_behaves_like "having area table"
+    end
+  end
+
+  describe "#pref_by_city" do
+    context "before call" do
+      it_behaves_like "not having area table"
+    end
+
+    context "after call" do
+      before { @client.send(:pref_by_city) }
+      it_behaves_like "having area table"
     end
   end
 
@@ -44,6 +67,31 @@ describe "WeatherHacker::Client" do
     it "return city_id" do
       city_id = @client.send(:city_id_by_zipcode, @zipcode)
       city_id.should be_kind_of Integer
+    end
+  end
+
+  describe "#id_by_pref" do
+    it "is cached in instance variable" do
+      @client.instance_variable_get(:@id_by_pref).should be_nil
+      @client.send(:id_by_pref)
+      @client.instance_variable_get(:@id_by_pref).should be_kind_of Hash
+
+      @client.should_not_receive(:pref_by_city)
+      @client.send(:id_by_pref)
+    end
+
+    it "return table of id by pref" do
+      @client.send(:id_by_pref).should be_kind_of Hash
+    end
+  end
+
+  describe "#parse_area_table" do
+    context "when passed malformed hash response" do
+      it do
+        expect {
+          @client.send(:parse_area_table, {:unexpected => :hash})
+        }.to raise_error(WeatherHacker::Client::ParseError)
+      end
     end
   end
 end
